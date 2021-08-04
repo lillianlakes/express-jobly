f2b6f8fc7a5d562923f907cef55479935a5c5a10
Serena <serenaqkong@gmail.com>
2021-08-04 12:07:49 -0700

    added sad path to sql.test.js

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ app.use(function (req, res, next) {
 
 /** Generic error handler; anything unhandled goes here. */
 app.use(function (err, req, res, next) {
-  if (process.env.NODE_ENV !== "test") console.error(err.stack);
   const status = err.status || 500;
   const message = err.message;
+  if (process.env.NODE_ENV !== "test") console.error(err.stack);
 
   return res.status(status).json({
     error: { message, status },
--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -3,22 +3,9 @@ const {
   BadRequestError,
   UnauthorizedError,
 } = require("../expressError");
-const db = require("../db");
-const Company = require("../models/company");
-const User = require("../models/user");
 const { sqlForPartialUpdate } = require("./sql");
 
-const {
-  commonBeforeAll,
-  commonBeforeEach,
-  commonAfterEach,
-  commonAfterAll,
-} = require("../models/_testCommon");
-
-beforeAll(commonBeforeAll);
-beforeEach(commonBeforeEach);
-afterEach(commonAfterEach);
-afterAll(commonAfterAll);
+//TODO: why console.error from app.js
 
 describe("sqlForPartialUpdate", function () {
   test("works: keys available", function () {
@@ -32,79 +19,25 @@ describe("sqlForPartialUpdate", function () {
   });
 
   test("does not work: length of keys is zero", function () {
-    const dataToUpdate = { };
+    const dataToUpdate = {};
     const jsToSql = { name: "name", description: "description", numEmployees: "num_employees" };
 
-    console.log(`right before error.....`);
+    let result;
+    try {
+      result = sqlForPartialUpdate(dataToUpdate, jsToSql);
+    } catch (err) {
+      expect(err.message).toEqual("No data");
+      expect(err.status).toEqual(400);
+    }
+  });
+  
+  test("works: jsToSql is empty", function () {
+    const dataToUpdate = { name: "TestName" };
+    const jsToSql = {};
 
-    const result = sqlForPartialUpdate(dataToUpdate, jsToSql);
+    const { setCols, values } = sqlForPartialUpdate(dataToUpdate, jsToSql);
 
-    // expect(result.statusCode).toEqual(400);
-    expect(result).toThrow("No data");
-    // expect(values).toEqual(["TestName", "TestDescription", 10]);
+    expect(setCols).toEqual(`"name"=$1`);
+    expect(values).toEqual(["TestName"]);
   });
 });
-
-
-
-
-
-
-
-
-// const { setCols, values } = sqlForPartialUpdate(
-//   data,
-//   {
-//     firstName: "first_name",
-//     lastName: "last_name",
-//     isAdmin: "is_admin",
-//   });
-
-//   function sqlForPartialUpdate(dataToUpdate, jsToSql) {
-//     const keys = Object.keys(dataToUpdate);
-//     if (keys.length === 0) throw new BadRequestError("No data");
-  
-//     // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-//     const cols = keys.map((colName, idx) =>
-//         `"${jsToSql[colName] || colName}"=$${idx + 1}`,
-//     );
-  
-//     return {
-//       setCols: cols.join(", "),
-//       values: Object.values(dataToUpdate),
-//     };
-//   }
-
-
-// describe("createToken", function () {
-//   test("works: not admin", function () {
-//     const token = createToken({ username: "test", is_admin: false });
-//     const payload = jwt.verify(token, SECRET_KEY);
-//     expect(payload).toEqual({
-//       iat: expect.any(Number),
-//       username: "test",
-//       isAdmin: false,
-//     });
-//   });
-
-//   test("works: admin", function () {
-//     const token = createToken({ username: "test", isAdmin: true });
-//     const payload = jwt.verify(token, SECRET_KEY);
-//     expect(payload).toEqual({
-//       iat: expect.any(Number),
-//       username: "test",
-//       isAdmin: true,
-//     });
-//   });
-
-//   test("works: default no admin", function () {
-//     // given the security risk if this didn't work, checking this specifically
-//     const token = createToken({ username: "test" });
-//     const payload = jwt.verify(token, SECRET_KEY);
-//     expect(payload).toEqual({
-//       iat: expect.any(Number),
-//       username: "test",
-//       isAdmin: false,
-//     });
-//   });
-// });
